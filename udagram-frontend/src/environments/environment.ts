38b612ce8ac4de6ae5437fc45df69e996ab6f94a
Anteneh2121 <antukassaw1@gmail.com>
2022-09-24 08:41:19 -0700

    commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1cce38d4be444e66a186c06df52dffc-662956085.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
